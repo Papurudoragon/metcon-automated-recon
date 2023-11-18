ddfdd63ec163abac2ef23ee07e17fd6f90cca6c8
Papurudoragon <81523151+Papurudoragon@users.noreply.github.com>
2023-11-18 10:53:43 -0500

    update

--- src/subdomains.sh
+++ src/subdomains.sh
@@ -3,9 +3,23 @@
 # pass $domain flag from main
 domain=$1
 
+# added .bash_profile for pathing
+BASH_PROFILE="$HOME/.bash_profile"
+
+# Check if .bash_profile exists, if not create it
+if [ ! -f "$BASH_PROFILE" ]; then
+    touch "$BASH_PROFILE"
+    echo "Created $BASH_PROFILE"
+fi
+
+# Export PATH variable to .bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> "$BASH_PROFILE"
+
+# Source .bash_profile
+source "$BASH_PROFILE"
+
 # Source the configuration file
 source ./src/config.sh
-source ~/.bash_profile 
 
 
 # Using the variables, we can run several commands for subdomain enum
